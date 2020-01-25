985147f661eaf2b77f46e236985d532ce3af968e
Max <maxim.pershin@protonmail.com>
2020-01-25 18:51:55 +0300

    scss support added

---
+++ src/components/Root.vue
@@ -0,0 +1,15 @@
+<template>
+	<div class="Root">
+		
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'RootComponent'
+}
+</script>
+
+<style lang='scss'>
+	
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,15 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import RootComponent from '@/components/Root.vue'
 
 Vue.use(Router)
 
 export default new Router({
-   routes: [
-
-   ]
+    routes: [
+    	{
+    		path: "/",
+    		name: "root",
+    		component: RootComponent
+    	}
+    ]
 })
